94836f810c97e58ffcbf5ebe9d70aaddb399ed1a
DESKTOP-OU76AT9\Glenn <glenn-hoff@hotmail.com>
2024-10-31 14:48:01 +0100

    [ADDS Match minigame setup

---
+++ GameRunner/Assets/02-GameRunner/01-Scripts/Minigames/Games/Match/MatchMinigame.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+public class MatchMinigame : Minigame
+{
+    public override void Initialize(string gameData, int scoreMultiplier, Action<float> onGameFinished)
+    {
+
+    }
+}
